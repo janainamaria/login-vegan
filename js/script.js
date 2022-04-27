5be1968f9cf01606a004c35235f40c8a6122d1e5
janaina <janainamariamdm@gmail.com>
2022-04-27 20:52:35 -0300

    fim

--- js/script.js
+++ js/script.js
@@ -1,33 +1,36 @@
+let nome1 = formulario.nome.value
+let email1 = formulario.email.value
+let senha1= formulario.senha.value
+let select1 = formulario.select.value
+
+let nome = formulario.nome
+let email = formulario.email
+let senha = formulario.senha
+let select = formulario.select
+ 
 function validarFormulario(){
-    let nome1 = formulario.nome.value
-    let email1 = formulario.email.value
-    let senha1= formulario.senha.value
-    let select1 = formulario.select.value
-
-    let nome = formulario.nome
-    let email = formulario.email
-    let senha = formulario.senha
-    let select = formulario.select
-
     if (nome1 == '') {
-      nome.style.border = "1px solid red";
-      
-    }else{
-        nome.style.border = "none";
-    }
-    if (email1 == '') {
-        email.style.border = "1px solid red";
-    }else{
-        nome.style.border = "none";
-    }
-      if (senha1 == '') {
-        senha.style.border = "1px solid red";    
-    }else{
-        nome.style.border = "none";
-    }
-    if (select1 == '') {
-        select.style.border = "1px solid red";
-    }else{
-        nome.style.border = "none";
-    }
+        nome.style.border = "1px solid red";
+        alert ('preencha o campo nome!')
+      }else{
+          nome.style.border = "none"
+      }
+      if (email1 == '') {
+          email.style.border = "1px solid red";
+          alert ('Preencha o campo email!')
+      }else{
+          email.style.border = "none";
+      }
+        if (senha1 == '') {
+          senha.style.border = "1px solid red";
+          alert ('Preencha o campo senha!')    
+      }else{
+          senha.style.border = "none";
+      }
+      if (select1 == '') {
+          select.style.border = "1px solid red";
+          alert ('Preencha o campo select!')
+      }else{
+          select.style.border = "none";
+      }
 }
