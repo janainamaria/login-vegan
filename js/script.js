0ba9bf97d0ad81c1106642cdb40cd9598fa8cd93
janaina <janainamariamdm@gmail.com>
2022-04-28 13:21:06 -0300

    a

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
-let nome1 = formulario.nome.value
-let email1 = formulario.email.value
-let senha1= formulario.senha.value
-let select1 = formulario.select.value
-
-let nome = formulario.nome
-let email = formulario.email
-let senha = formulario.senha
-let select = formulario.select
-
-let camponome = document.getElementById("pnome")
-let campoemail = document.getElementById("pemail")
-let camposenha = document.getElementById("psenha")
 function validarFormulario(){
+    let nome1 = formulario.nome.value
+    let email1 = formulario.email.value
+    let senha1= formulario.senha.value
+    let select1 = formulario.select.value
+
+    let nome = formulario.nome
+    let email = formulario.email
+    let senha = formulario.senha
+    let select = formulario.select
+
+    let camponome = document.getElementById("pnome")
+    let campoemail = document.getElementById("pemail")
+    let camposenha = document.getElementById("psenha")
+
     if (nome1 == '') {
         nome.style.border = "1px solid red";
         camponome.innerHTML = 'Preencha este campo!'
       }else{
           nome.style.border = "none"
+          camponome.style.display = "none"
       }
       if (email1 == '') {
           email.style.border = "1px solid red";
